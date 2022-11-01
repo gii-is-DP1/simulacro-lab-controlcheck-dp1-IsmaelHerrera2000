14a365f88a682ac56af2f79ee1ebc52adde72cc3
ismherram <ismherram@alum.us.es>
2022-11-01 16:19:45 +0100

    Hecho

--- src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
@@ -1,15 +1,28 @@
 package org.springframework.samples.petclinic.product;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 
 
-public interface ProductRepository {
-    List<Product> findAll();
+public interface ProductRepository extends CrudRepository<Product, Integer>{
+	
+    //Ejercicio 2
+    @Query("SELECT producttype FROM ProductType producttype")
     List<ProductType> findAllProductTypes();
+  //Ejercicio5
+    List<Product> findAll();
+  //Ejercicio 6
+    @Query("SELECT p FROM ProductType p WHERE p.name =?1")
+    ProductType getProductType(String name);
+    //Ejercicio8
+    @Query("SELECT p FROM Product p WHERE p.price < ?1")
+    List<Product> findByLessThan(Double price);
+    
     Optional<Product> findById(int id);
     Product findByName(String name);
     Product save(Product p);
